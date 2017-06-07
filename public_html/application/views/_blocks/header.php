b9e51e6eef92d6d822161af42ae194152a27e104
Carlos Orozco <carlos.orozco@sekologistics.com>
2017-06-07 17:26:19 -0500

    se agrega el phpadmin

--- public_html/application/views/_blocks/header.php
+++ public_html/application/views/_blocks/header.php
@@ -3,21 +3,17 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="Responsive Bootstrap Landing Page Template">
-    <meta name="keywords" content="Bootstrap, Landing page, Template, Registration, Landing">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <meta name="author" content="Grayrids">
-    <title>Bienvenidos a su sitio</title>
-    <link href="template/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Font Awesome CSS -->
-    <link rel="stylesheet" href="template/fonts/font-awesome.min.css" type="text/css" media="screen">
-    <!-- Include roboto.css to use the Roboto web font, material.css to include the theme and ripples.css to style the ripple effect -->
-    <link href="<?php echo base_url('template/css/material.min.css') ?>" rel="stylesheet">
-    <link href="<?php echo base_url('template/css/ripples.min.css') ?>" rel="stylesheet">
-    <link href="<?php echo base_url('template/css/main.css') ?>" rel="stylesheet">
-    <link href="<?php echo base_url('template/css/responsive.css') ?>" rel="stylesheet">
-    <link href="<?php echo base_url('template/css/animate.min.css') ?>" rel="stylesheet">
+    <title>Sistema de Boletaje</title>
+      <!-- Material Design fonts -->
+      <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700">
+      <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/icon?family=Material+Icons">
 
+      <!-- Bootstrap -->
+      <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+      <!-- Bootstrap Material Design -->
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap-material-design.css') ?> ">
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/ripples.min.css') ?> ">
 </head>
 <body>
     
--- public_html/application/views/home_view.php
+++ public_html/application/views/home_view.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Responsive Bootstrap Landing Page Template">
+    <meta name="keywords" content="Bootstrap, Landing page, Template, Registration, Landing">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="author" content="Grayrids">
+    <title>Bienvenidos a su sitio</title>
+    <link href="template/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="template/fonts/font-awesome.min.css" type="text/css" media="screen">
+    <!-- Include roboto.css to use the Roboto web font, material.css to include the theme and ripples.css to style the ripple effect -->
+    <link href="<?php echo base_url('template/css/material.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('template/css/ripples.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('template/css/main.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('template/css/responsive.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('template/css/animate.min.css') ?>" rel="stylesheet">
+
+</head>
+<body>
+    
 
 
     <div class="navbar navbar-invers menu-wrap">
@@ -424,4 +447,43 @@
       <i class=" mdi-hardware-keyboard-arrow-up"></i>
       </a>
     </section> 
+
+
+    <section id="copyright">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <p class="copyright-text">
+             © ExpertTyCE 2017 Todos los derechos reservados. 
+              <a rel="nofollow" href="http://experttyce.com/">
+                ExpertTyCE
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>     
+    
+        
+
+    <script src="<?php echo base_url('template/js/jquery-2.1.4.min.js') ?>"></script>
+    <script src="<?php echo base_url('template/js/bootstrap.min.js') ?>"></script>
+    <script src="<?php echo base_url('template/js/ripples.min.js') ?>"></script>
+    <script src="<?php echo base_url('template/js/material.min.js') ?>"></script>
+    <script src="<?php echo base_url('template/js/wow.js') ?>"></script>
+    <script src="<?php echo base_url('template/js/jquery.mmenu.min.all.js') ?>"></script> 
+    <script src="<?php echo base_url('template/js/count-to.js') ?>"></script>   
+    <script src="<?php echo base_url('template/js/jquery.inview.min.js') ?>"></script>     
+    <script src="<?php echo base_url('template/js/classie.js') ?>"></script>
+    <script src="<?php echo base_url('template/js/jquery.nav.js') ?>"></script>      
+    <script src="<?php echo base_url('template/js/smooth-on-scroll.js') ?>"></script>
+    <script src="<?php echo base_url('template/js/smooth-scroll.js') ?>"></script>
+    <script src="<?php echo base_url('template/js/main.js') ?>"></script>
+    <script src="<?php echo base_url('template/js/core.js') ?>"></script>
+    <!-- <script src="template/js/core.js"></script> -->
+    
+  </body>
+
+</html>
+
     
